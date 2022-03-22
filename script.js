509598ed265c1fdd3717d3df82e74bdbf6464521
Mylena Vitoriano <71454293+mylenavitoriano@users.noreply.github.com>
2022-03-22 20:41:41 -0300

    ajustes finais de pasta realizados

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function imgSlider(anything, color){
-    document.querySelector('.starbucks').src = '../images/'+anything
+    document.querySelector('.starbucks').src = 'images/'+anything
 
     const circle = document.querySelector('.circle')
     circle.style.backgroundColor = color
@@ -14,9 +14,9 @@ function mudar_tema(background, color, display_dark, display_light, imagem=""){
     }
 
     document.querySelector('.textBox').style.color = color
-    document.querySelector('#facebook').src = `../images/facebook${imagem}.svg`
-    document.querySelector('#twitter').src = `../images/twitter${imagem}.svg`
-    document.querySelector('#instagram').src = `../images/instagram${imagem}.svg`
+    document.querySelector('#facebook').src = `images/facebook${imagem}.svg`
+    document.querySelector('#twitter').src = `images/twitter${imagem}.svg`
+    document.querySelector('#instagram').src = `images/instagram${imagem}.svg`
 
     document.querySelector('img.dark').style.display = display_dark
     document.querySelector('img.light').style.display = display_light
