8a56785571504f89f3a0825a79d6b58eebcdb494
Mylena Vitoriano <71454293+mylenavitoriano@users.noreply.github.com>
2022-03-24 20:04:55 -0300

    criado função de clique do menu hamburguer e melhorado adicionado as novas funcionalidades de tema para os novos elementos

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function imgSlider(anything, color){
     document.querySelector('.circle').style.backgroundColor = color
 }
 
-function mudar_tema(background, color, display_dark, display_light, imagem=""){
+function mudar_tema(background, color, display_dark, display_light, imagem="", filter=1){
     document.querySelector('section').style.backgroundColor = background
 
     const nav = document.querySelectorAll('header ul li a')
@@ -17,7 +17,18 @@ function mudar_tema(background, color, display_dark, display_light, imagem=""){
     document.querySelector('#twitter').src = `images/twitter${imagem}.svg`
     document.querySelector('#instagram').src = `images/instagram${imagem}.svg`
 
+    document.querySelector('.navigation').style.background = background
+    document.querySelector('.toggle').style.filter = "invert("+filter+")"
+
     document.querySelector('img.dark').style.display = display_dark
     document.querySelector('img.light').style.display = display_light
+}
 
+function toggleMenu(){
+    var menuToggle = document.querySelector('.toggle')
+    var navbar = document.querySelector('.navbar')
+    var navigation = document.querySelector('.navigation')
+    menuToggle.classList.toggle('active')
+    navbar.classList.toggle('active')
+    navigation.classList.toggle('active')
 }
